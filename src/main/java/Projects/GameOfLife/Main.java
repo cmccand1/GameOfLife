72cc38bb5f853df73329ac58541b215c2c254cfd
cmccand1 <clint.mccandless@gmail.com>
2024-03-11 18:36:27 -0700

    Refactor main method in GameOfLife project
    The main method has been refactored in the GameOfLife project. Specific classes were imported instead of importing the entire swing and awt packages, and model, view, and controller instances were changed to use factory methods. Modeling is done with a createModel() method, the view now uses a noarg constructor, and the controller now uses a method that includes a random initialization.

--- src/main/java/Projects/GameOfLife/Main.java
+++ src/main/java/Projects/GameOfLife/Main.java
@@ -6,15 +6,17 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        Model model = new Model();
-        View view = new View(model);
-        Controller controller = new Controller(model, view);
+        Model model = Model.createModel();
+        View view = new View();
+        Controller controller = Controller.createControllerWithRandomInit(model, view);
+        //Controller controller = Controller.createController(model, view);
 
         JFrame frame = new JFrame("Game of Life");
         JButton startButton = new JButton("Start");
         startButton.addActionListener(e -> {
             controller.simulate();
             startButton.setEnabled(false);
+
         });
         frame.add(view, BorderLayout.CENTER);
         frame.add(startButton, BorderLayout.SOUTH);
